7c2f0333d60e058f94f8c810f2df1c7c1a4d0473
Brianna Bukowski <bmbukowski@stu.naperville203.org>
2021-03-02 13:25:38 -0600

    updates

--- client.py
+++ client.py
@@ -1,3 +1,7 @@
-while #ultrasonic sensor does not detect anything close
+while 
+    #moves to location based on GPS coordinate
+    #ultrasonic sensor does not detect anything close
+    #moves to location based on GPS coordinate
+    #ultrasonic sensor does not detect anything close
     #run loop
     #test sensors
--- movement.py
+++ movement.py
@@ -1,14 +1,22 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import gps
 
 # Pins for Motor Driver Inputs 
+
+# left side
 Motor1A = 24
 Motor1B = 23
 Motor1E = 25
+pwm1=GPIO.PWM(25, 1000) #PWM for left motor
+pwm1.start(0) #start at 0% (off)
 
+# right side
 Motor2A = 10
 Motor2B = 9
 Motor2E = 11
+pwm2=GPIO.PWM(11, 1000) #PWM for right motor
+pwm2.start(0) #start at 0% (off)
 
 def stop():
     GPIO.output(Motor1E,GPIO.LOW)
@@ -23,29 +31,41 @@ def setup():
 	GPIO.setup(Motor2B,GPIO.OUT)
     GPIO.setup(Motor2E,GPIO.OUT)
  
-def forward():
+#speed is an integer from 0-100 for the speed of the motor using PWM.
+
+def forward(speed):
     #go forward
     GPIO.output(Motor1A,GPIO.HIGH)
 	GPIO.output(Motor1B,GPIO.LOW)
-    GPIO.output(Motor1E,GPIO.HIGH)
+    pwm1.ChangeDutyCycle(speed)
     GPIO.output(Motor2A,GPIO.HIGH)
 	GPIO.output(Motor2B,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.HIGH)
+    pwm2.ChangeDutyCycle(speed)
 
-def backward():
+def backward(speed):
     #go backward
     GPIO.output(Motor1A,GPIO.LOW)
 	GPIO.output(Motor1B,GPIO.HIGH)
-    GPIO.output(Motor1E,GPIO.HIGH)
+    pwm1.ChangeDutyCycle(speed)
     GPIO.output(Motor2A,GPIO.LOW)
 	GPIO.output(Motor2B,GPIO.HIGH)
-    GPIO.output(Motor2E,GPIO.HIGH)
+    pwm2.ChangeDutyCycle(speed)
 
-def right():
-    #turn right
+def right(speed):
+    GPIO.output(Motor1A,GPIO.HIGH)
+	GPIO.output(Motor1B,GPIO.LOW)
+    pwm1.ChangeDutyCycle(speed)
+    GPIO.output(Motor2A,GPIO.LOW)
+	GPIO.output(Motor2B,GPIO.HIGH)
+    pwm2.ChangeDutyCycle(speed)
 
-def left():
-    #turn left
+def left(speed):
+    GPIO.output(Motor1A,GPIO.LOW)
+	GPIO.output(Motor1B,GPIO.HIGH)
+    pwm1.ChangeDutyCycle(speed)
+    GPIO.output(Motor2A,GPIO.HIGH)
+	GPIO.output(Motor2B,GPIO.LOW)
+    pwm2.ChangeDutyCycle(speed)
 
 def turnAround():
     #180 degree turn and like back up kinda beat
