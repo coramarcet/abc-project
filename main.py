e61ec35faf0020976adb5f9be6774b623fddf43b
adriantse136 <54153247+adriantse136@users.noreply.github.com>
2021-05-14 12:38:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-#this is middleman
+from wormonastring import getSerial
+import time
 
 def setup():
-    print ('Program is starting...')
-    GPIO.setmode(GPIO.BOARD) #numbers GPIOs by physical location
-    GPIO.setup(trigPin, GPIO.OUT) # set trigPin to output mode
-    GPIO.setup(echoPin, GPIO.IN) # set echoPin to input mode
+    None
+#     GPIO.setmode(GPIO.BOARD) #numbers GPIOs by physical location
+#     GPIO.setup(trigPin, GPIO.OUT) # set trigPin to output mode
+#     GPIO.setup(echoPin, GPIO.IN) # set echoPin to input mode
 
 def loop():
     while(True):
-        distance = getSonar()
-        #print ("Tdhe distance is : %.2f cm"%(distance))
-        if distance < 25:
-            right(50)
-        else:
-            forward(50)
+#         distance = getSonar()
+#         print ("The distance is : %.2f cm"%(distance))
+#         if distance < 25:
+#             right(50)
+#         else:
+#             forward(50)
+        
+        obj = getSerial()
+        print(obj.getpH())
+        
         time.sleep(1)
 
-def __name__ == '__main__': #program start from here
+def __init__(): #program start from here
     setup()
     try:
         loop()
     except KeyboardInterrupt:
         GPIO.cleanup()
+__init__()
