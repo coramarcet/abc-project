cb80fd4b7d2dfa8829e0ead17e0bba39ce84b802
adriantse136 <54153247+adriantse136@users.noreply.github.com>
2021-05-14 12:34:06 -0500

    Update wormonastring.py

--- wormonastring.py
+++ wormonastring.py
@@ -1,1 +1,19 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0')
+
+class getSerial:
+    def __init__(self):
+        ser.flushInput()
+    
+    def getpH(self):
+        ser_bytes = ser.readline()
+        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-1].decode("utf-8"))
+        return(decoded_bytes)
+    
+# obj = getSerial()
+# for i in range(100):
+#     print(obj.getpH())
+#     time.sleep(0.5)
 
