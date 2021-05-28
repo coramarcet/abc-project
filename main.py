75d410f4c59a787fcf825b5331f383718eef45c2
adriantse136 <54153247+adriantse136@users.noreply.github.com>
2021-05-28 12:21:39 -0500

    Variable names

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import time
 def setup():
     obj = getSerial() #pH object
     ultra = sonic() #ultrasonic sensor object
-    distance = sonic.getSonar() #distance variable for ultrasonic sensor
+    distance = ultra.getSonar() #distance variable for ultrasonic sensor
     swim = move() #movement object
 
 def run():
     while(True):
         #determines distance from nearest object
-        distance = sonic.getSonar()
+        distance = ultra.getSonar()
         while(distance < 20):
             swim.right(100)
             time.sleep(1)
             swim.stop()
-            distance = sonic.getSonar()
+            distance = ultra.getSonar()
         
         #once there is no object in way, moves forward
         swim.forward(100)
